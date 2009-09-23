fd4519fde223f6a5fb4ca5db1fe1a7b84c28bc7b
David Cramer <dcramer@David-Cramers-MacBook.local>
2009-09-23 11:33:06 -0500

    Allow uppercase characters in profile urls

--- pastethat/profiles/urls.py
+++ pastethat/profiles/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import *
 import views
 
 urlpatterns = patterns('',
-    url(r'^/(?P<username>[a-z0-9_-]+)/pastes$', views.pastes, name='profiles.pastes'),
-    url(r'^/(?P<username>[a-z0-9_-]+)$', views.details, name='profiles.details'),
+    url(r'^/(?P<username>[a-zA-Z0-9_-]+)/pastes$', views.pastes, name='profiles.pastes'),
+    url(r'^/(?P<username>[a-zA-Z0-9_-]+)$', views.details, name='profiles.details'),
 )
