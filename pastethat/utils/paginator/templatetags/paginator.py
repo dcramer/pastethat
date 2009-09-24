932d7819904439cf4503990ef67a7cdfaed2f344
David Cramer <dcramer@David-Cramers-MacBook.local>
2009-09-23 22:26:56 -0500

    mark_safe doesnt work with Markup

--- pastethat/utils/paginator/templatetags/paginator.py
+++ pastethat/utils/paginator/templatetags/paginator.py
@@ -1,4 +1,3 @@
-from django.utils.safestring import mark_safe
 from pastethat.utils.paginator.paginator import EndlessPaginator
 from pastethat.utils.template.shortcuts import render_to_string
 from jinja2 import Markup
@@ -16,6 +15,6 @@ def paginate(request, queryset_or_list, limit=25, paginator_class=EndlessPaginat
         'query_string': query_dict.urlencode(),
         'paginator': paginator.get_context(request.GET.get('p', 1)),
     }
-    paging = mark_safe(Markup(render_to_string('bone/paging.html', context)))
+    paging = Markup(render_to_string('bone/paging.html', context))
     return dict(objects=context['paginator'].get('objects', []), paging=paging)
 register.object(paginate)
