80ea1cb2c74edc93301e58d4c73c63edcacd8bfd
David Cramer <dcramer@gmail.com>
2010-07-29 12:07:23 -0700

    only enable media and static urls in debug mode

--- pastethat/urls.py
+++ pastethat/urls.py
@@ -3,10 +3,17 @@ from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+if settings.DEBUG:
+    urlpatterns = patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        url(r'^static/(?:\d+/)?(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    )
+else:
+    urlpatterns = patterns('')
 
+urlpatterns += patterns('',
     (r'^admin/(.*)', admin.site.root),
     (r'^account', include('pastethat.accounts.urls')),
     (r'^users', include('pastethat.profiles.urls')),
