c74d9ac82483bf63e8d25eb0c5cd6fa71a6ee86f
David Cramer <dcramer@gmail.com>
2010-08-03 11:07:00 -0700

    Fix for non-www domain handling without htaccess

--- pastethat/pastes/middleware.py
+++ pastethat/pastes/middleware.py
@@ -4,6 +4,9 @@ from models import Group
 
 class GroupProcessorMiddleware(object):
     def process_request(self, request):
+        if request.META['HTTP_HOST'] == settings.BASE_DOMAIN:
+            # Handles the non-www case
+            return HttpResponseRedirect('http://www.%s%s' % (settings.BASE_DOMAIN, request.get_full_path()))
         settings.GROUP_DOMAIN = request.META['HTTP_HOST'].replace('.' + settings.BASE_DOMAIN, '')
         if settings.BASE_DOMAIN:
             settings.BASE_URL = 'http://%s.%s' % (settings.GROUP_DOMAIN, settings.BASE_DOMAIN)
